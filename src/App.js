693b7a3e2d0a75d304ec8d7dbba0e8806ef4833a
iconicsudip <das366966@gmail.com>
2022-11-18 00:12:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState,useEffect} from 'react';
+import SignIn from './components/SignIn'
+import SignUp from './components/SignUp';
+import axios from "axios";
+import { BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+import Home from './components/Home';
+import DashBoard from './components/DashBoard';
+import NavBar from './components/NavBar';
+import MyAccount from './components/MyAccount';
 
 function App() {
+  const [user,setUser] = useState(null);
+  console.log(process.env.REACT_APP_SERVER_URL+"/auth/login/success")
+  const getUser = async ()=>{
+    try{
+      const url = process.env.REACT_APP_SERVER_URL+"/auth/login/success";
+      const data = await axios.get(url, { withCredentials: true });
+      setUser(data.data.user)
+    }catch(err){
+      console.log(err);
+    }
+  }
+  useEffect(()=>{
+    getUser();
+  },[])
+  // console.log(user)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavBar user={user}/>
+        <Routes>
+          <Route exact path="/" element={<Home />}/>
+          <Route exact path="/signin" element={user ? <Navigate to="/dashboard" />:<SignIn />}/>
+          <Route exact path="/signup" element={user ? <Navigate to="/dashboard" />:<SignUp />}/>
+          <Route exact path="/myaccount" element={user ? <MyAccount user={user}/>:<Navigate to="/signin" />}/>
+          <Route exact path="/dashboard" element={user ? <DashBoard />:<Navigate to="/signin" />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
